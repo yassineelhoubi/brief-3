628ef852159154cd4b5ea30b1dd847a90b43cb0f
yassineelhoubi <77664568+yassineelhoubi@users.noreply.github.com>
2021-02-23 14:01:50 +0100

    Brief-3

---
+++ brief-3/brief 3/script-contact.js
@@ -0,0 +1,9 @@
+function xfunction(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+    alert(" Hello mr/mrs : " + name  + "\n your account email : " + email + "\n Your number phone : " + phone + "\n your message is : " + message);
+  
+}
+
